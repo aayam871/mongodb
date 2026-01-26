5b229989e6e6e8496205ad8af93e72e19e1b8628
Aayam Regmi <aayamregmi81@gmail.com>
2026-01-26 16:49:26 +0545

    feat(auth): implement JWT auth, store token in localStorage, protect /home, redirect unauthenticated users

--- userModel.js
+++ userModel.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/loginsignupDB");
 
+
 const userSchema = mongoose.Schema({
   firstName: String,
   lastName: String,
   address: String,
   email: String,
-  password: String, // ahile simple rakheko (no hashing)
+  password: String, 
 });
 
 module.exports = mongoose.model("User", userSchema);
