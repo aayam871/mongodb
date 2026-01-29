48312bc49da0729bc8f23976d3db68f245749ec7
Aayam Regmi <aayamregmi81@gmail.com>
2026-01-29 13:01:27 +0545

    usage of multer for file uploads

--- userModel.js
+++ userModel.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/loginsignupDB");
-
+mongoose.connect("mongodb://127.0.0.1:27017/multeruseDB");
 
 const userSchema = mongoose.Schema({
   firstName: String,
   lastName: String,
   address: String,
   email: String,
-  password: String, 
+  password: String,
+  photo: String,
 });
 
 module.exports = mongoose.model("User", userSchema);
